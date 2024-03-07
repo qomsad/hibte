dbfa7b1da06181fdd64ba8a86c51f11aa11fecca
qomsad <qomsad@users.noreply.github.com>
2024-03-07 22:24:03 +0500

    del

--- src/main/java/tech/hibte/c2/C2Repository.java
+++
@@ -1,9 +0,0 @@
-package tech.hibte.c2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tech.hibte.c2.model.entity.ArentC2;
-
-@Repository
-public interface C2Repository extends JpaRepository<ArentC2, String> {
-}
